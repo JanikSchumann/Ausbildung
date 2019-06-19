a3066d950399ee0cccf58b172637dee7039a0db4
JanikSchumann <49280965+JanikSchumann@users.noreply.github.com>
2019-06-19 10:57:54 +0200

    Add files via upload

---
+++ dotdotdot.js
@@ -0,0 +1,12 @@
+function dotdotdot(input_string, counter) {
+   input_string = input_string.substring(0, (counter-3));
+   var i = input_string.length
+   while (input_string[i] != " "){
+     i--;
+   }
+   input_string = input_string.substring(0, i);
+   input_string += "...";
+   return input_string;
+}
+
+dotdotdot('Das ist ein Test-Satz der wirklich sehr lang geschrieben ist.', 30);
